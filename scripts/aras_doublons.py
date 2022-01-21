f77183c70eda22d4a82a105d98978d06eb0f170b
FMTeyssier <francoismathieu.teyssier@gmail.com>
2022-01-21 12:06:42 +0100

    2021-01-21
    Doublons deleted

--- scripts/aras_doublons.py
+++ scripts/aras_doublons.py
@@ -1,45 +1,12 @@
-from astropy.io import ascii
+# delete duplicates (criterions: objname, jd, observer)
+# and sort all_spectra by objname and jd
+# fmt - 2022-01-21
+
 import pandas as pd
 
+df = pd.read_csv('../data/all_spectra.csv', delimiter=";")
+df =df.sort_values(by=["star_name_string","jd"])
+df = df.drop_duplicates(subset=["star_name_string", "jd","observer"], keep='last')
+df.to_csv('../data/all_spectra.csv',index=False,sep = ';')
 
 
-
-
-all_spectra = ascii.read("../data/all_spectra.csv", header_start=0, data_start=1, delimiter=';',format='csv')
-try:
-    all_spectra.rename_column('ď»żstar_name_string', 'star_name_string')
-except:
-    pass
-
-symbiotic_stars = ascii.read("../data/symbiotic_stars.csv", header_start=0, data_start=1, delimiter=';',format='csv')
-try:
-    symbiotic_stars.rename_column('ď»żstar_name_string', 'star_name_string')
-except:
-    pass
-
-nb =0
-
-# df = pd.read_csv('../data/all_spectra.csv', delimiter=";")
-# df1 =df.sort_values(by=["star_name_string","jd"])
-# print(df1)
-
-df1 = pd.read_csv('../data/all_spectra_sort.csv', delimiter=",")
-
-
-
-
-
-nLines = len(df1)
-print(nLines)
-for n in range(1,nLines):
-    if df1.jd[n]==df1.jd[n-1]:
-    # if df1.file[n]==df1.file[n-1]:
-    # if df1.date[n]==df1.date[n-1] and df1.time[n]==df1.time[n-1]:    
-        nb = nb+1
-        print(nb)
-        print(n)
-        print(df1.file[n-1])
-        print(df1.file[n])
-        
-     
-df1.to_csv('../data/all_spectra_sort.csv',index=False)
