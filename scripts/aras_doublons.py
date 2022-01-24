66da1cddad285f03084eeda4c65fd3925742a280
FMTeyssier <francoismathieu.teyssier@gmail.com>
2022-01-24 09:13:10 +0100

    2021-01-24

--- scripts/aras_doublons.py
+++ scripts/aras_doublons.py
@@ -7,6 +7,6 @@ import pandas as pd
 df = pd.read_csv('../data/all_spectra.csv', delimiter=";")
 df =df.sort_values(by=["star_name_string","jd"])
 df = df.drop_duplicates(subset=["star_name_string", "jd","observer"], keep='last')
-df.to_csv('../data/all_spectra.csv',index=False,sep = ';')
+# df.to_csv('../data/all_spectra.csv',index=False,sep = ';')
 
 
