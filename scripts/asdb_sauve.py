4f39a7f7e30b4fcbb10de5aafb1f98e194be2b09
FMTeyssier <francoismathieu.teyssier@gmail.com>
2022-10-24 09:33:46 +0200

    2022-10-24

---
+++ scripts/asdb_sauve.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 23 12:57:40 2022
+Sauvegarde asdb
+"""
+
+import shutil 
+
+src = r"C:\Users\franc\OneDrive\Documents\GitHub\database"
+dest = r"D:\asdb_aras"
+shutil.copytree(src, dest) 
