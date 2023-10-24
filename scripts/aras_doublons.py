dfe52e8ffb889200548a1f3f6ee008418d6cd8be
FMTeyssier <francoismathieu.teyssier@gmail.com>
2023-10-24 14:47:04 +0200

    2023-10-24

--- scripts/aras_doublons.py
+++ scripts/aras_doublons.py
@@ -6,10 +6,12 @@ import pandas as pd
 
 
 df = pd.read_csv(r'C:\Users\franc\OneDrive\Documents\GitHub\database\data/all_spectra.csv', delimiter=";")
+n0=len(df.index)
 df =df.sort_values(by=["star_name_string","jd"])
 df = df.drop_duplicates(subset=["star_name_string", "jd","observer"], keep='last')
 df.to_csv('../data/all_spectra.csv',index=False,sep = ';')
-
+n1=len(df.index)
+print("nb doublons removed = ",n0-n1)
 
 
     
