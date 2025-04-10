97980bc6fa54ba660a50f63afad9a9737f6d5477
FMTeyssier <francoismathieu.teyssier@gmail.com>
2025-04-10 18:01:56 +0200

    *

--- scripts/asdb_CreateObject.py
+++ scripts/asdb_CreateObject.py
@@ -4,11 +4,11 @@ from astroquery.simbad import Simbad
 import os
 
 #data
-ObjName = "[DWS97] Cet 7" 
+ObjName = "RX Pup" 
 Program = "ARAS Symbiotics Program"
 File0 = 'symbiotic_stars.csv'
 hr = "-" # Priority HR
-lr = "2" # Priority LR
+lr = "1" # Priority LR
 cadency = 30
 
 # Mise en forme
