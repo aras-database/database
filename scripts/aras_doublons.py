af7180cffbf6ee3396967ea3bbb8b1afc91848d4
FMTeyssier <francoismathieu.teyssier@gmail.com>
2025-08-21 22:24:55 +0200

    *

--- scripts/aras_doublons.py
+++ scripts/aras_doublons.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 # Spectra
 
-df = pd.read_csv(r'C:\Users\franc\OneDrive\Documents\GitHub\database\data/all_spectra.csv', delimiter=";")
+df = pd.read_csv(r'C:\Users\franc\Documents\GitHub\database\data\all_spectra.csv', delimiter=";")
 n0=len(df.index)
 df =df.sort_values(by=["star_name_string","jd"])
 df = df.drop_duplicates(subset=["star_name_string", "jd","observer"], keep='last')
