a39bf39ac7bd404b1a3121891653c0417373a67b
FMTeyssier <francoismathieu.teyssier@gmail.com>
2026-09-21 08:21:16 +0200

    *

--- scripts/aras_novae_preprocess.py
+++ scripts/aras_novae_preprocess.py
@@ -13,14 +13,9 @@ os.chdir(r'C:\Users\franc\Documents\GitHub\database\new_spectra\novae')
 cwd = os.getcwd() 
 path= cwd
 
-
-
-
 #data = ascii.read("names.csv", header_start=0, data_start=1, delimiter=';',format='csv')
 
-
 files=glob.glob("*.fit*")  
-
 
 print(files)
             
